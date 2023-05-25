36762e8af9a6d373585d9532705556d5cad3213a
gaston <gastonvallasciani@gmail.com>
2023-05-24 23:42:24 -0300

    Update platform.ini
    Configuration:
    	platform = espressif32
    	board = esp32dev
    	framework = espidf
    	monitor_speed = 115200
    	Upload_port = COM3
    	build_type = release
    	board_upload.flash_size = 4MB
    	monitor_filters = esp32_exception_decoder

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "../include/led_manager.h"
 
 void app_main() 
 {
     led_manager_init();
+    
+    while(true) 
+    {
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
 }
