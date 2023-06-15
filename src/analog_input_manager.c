a91cd8379c50afecdc8d06a6431a308e71c6bd62
gaston <gastonvallasciani@gmail.com>
2023-06-15 01:31:59 -0300

    Update analog reading manager

--- src/analog_input_manager.c
+++ src/analog_input_manager.c
@@ -31,33 +31,40 @@ static void config_analog_input(void);
 
 //------------------- DEFINICION DE DATOS GLOBALES -----------------------------
 //------------------------------------------------------------------------------
+adc_oneshot_unit_handle_t adc1_handle;
 
 //------------------- DEFINICION DE FUNCIONES LOCALES --------------------------
 //------------------------------------------------------------------------------
 static void config_analog_input(void)
 {
     //-------------ADC1 Init---------------//
-    adc_oneshot_unit_handle_t adc1_handle;
     adc_oneshot_unit_init_cfg_t init_config1 = {
-        .unit_id = ADC_UNIT_1,
+        .unit_id = ADC_UNIT_2,
     };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+    adc_oneshot_new_unit(&init_config1, &adc1_handle);
 
      //-------------ADC1 Config---------------//
     adc_oneshot_chan_cfg_t config = {
         .bitwidth = ADC_BITWIDTH_DEFAULT,
         .atten = ADC_ATTEN,
     };
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_POTE_INPUT, &config));
+    adc_oneshot_config_channel(adc1_handle, ADC_POTE_INPUT, &config);
 
 }
 //------------------------------------------------------------------------------
 static void analog_input_manager_task(void* arg)
 {
+    int raw_value = 0;
+
     config_analog_input();
 
     while(1)
     {
+        adc_oneshot_read(adc1_handle, ADC_POTE_INPUT, &raw_value);
+        #ifdef DEBUG_MODULE
+            printf("Valor ADC canal 5: %d \n", raw_value);
+        #endif
+        vTaskDelay(pdMS_TO_TICKS(1000));
 
     }
 }
@@ -66,7 +73,7 @@ static void analog_input_manager_task(void* arg)
 void analog_input_manager_init(void)
 {
     xTaskCreate(analog_input_manager_task, "analog_input_manager_task", 
-        configMINIMAL_STACK_SIZE*2, NULL, configMAX_PRIORITIES-2, NULL);
+        configMINIMAL_STACK_SIZE*4, NULL, configMAX_PRIORITIES-2, NULL);
 }
 //---------------------------- END OF FILE -------------------------------------
 //------------------------------------------------------------------------------
