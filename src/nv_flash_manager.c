f1119239f2321f5dfc6ff7a74eb8a2f557da2dec
gaston <gastonvallasciani@gmail.com>
2023-07-08 18:15:11 -0300

    New feature: save parameters on nv_flash

---
+++ src/nv_flash_manager.c
@@ -0,0 +1,86 @@
+//--------------------INCLUDES--------------------------------------------------
+//------------------------------------------------------------------------------
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "nvs_flash.h"
+#include "nvs.h"
+
+#include "../include/board_def.h"
+#include "../include/nv_flash_manager.h"
+//--------------------MACROS Y DEFINES------------------------------------------
+//------------------------------------------------------------------------------
+#define DEBUG_MODULE
+//------------------- TYPEDEF --------------------------------------------------
+//------------------------------------------------------------------------------
+
+//------------------- DECLARACION DE DATOS LOCALES -----------------------------
+//------------------------------------------------------------------------------
+
+//------------------- DECLARACION DE FUNCIONES LOCALES -------------------------
+//------------------------------------------------------------------------------
+static void first_time_flash(void);
+//------------------- DEFINICION DE DATOS LOCALES ------------------------------
+//------------------------------------------------------------------------------
+
+//------------------- DEFINICION DE DATOS GLOBALES -----------------------------
+//------------------------------------------------------------------------------
+
+//------------------- DEFINICION DE FUNCIONES LOCALES --------------------------
+//------------------------------------------------------------------------------
+// TO DO: terminar con la inicializacion de datos
+// TO DO: resolver el problema de la primera lectura de date erronea en flash
+
+static void first_time_flash(void)
+{
+    struct tm time_info_default;
+
+    time_info_default.tm_year = 123;
+    time_info_default.tm_mon = 6;
+    time_info_default.tm_mday = 8;
+    time_info_default.tm_hour = 13;
+    time_info_default.tm_min = 30;
+    time_info_default.tm_sec = 30;
+
+    init_parameter_in_flash_str(DEVICE_ALIAS_KEY, DEVICE_ALIAS_DEFAULT);
+    init_parameter_in_flash_str(WIFI_AP_SSID_KEY, WIFI_AP_SSID_DEFAULT);
+
+    init_date_parameter_in_flash(PWM_DATE_OFF_KEY, time_info_default);// work around
+    init_date_parameter_in_flash(PWM_DATE_ON_KEY, time_info_default);
+    time_info_default.tm_hour++;
+    init_date_parameter_in_flash(PWM_DATE_OFF_KEY, time_info_default);
+
+    init_parameter_in_flash_uint32(PWM_MODE_KEY, PWM_MODE_DEFAULT);
+    init_parameter_in_flash_uint32(SIMUL_DAY_STATUS_KEY, PWM_SIMUL_DAY_STATUS_DEFAULT);
+    init_parameter_in_flash_uint32(PWM_PERCENT_POWER_KEY, PWM_PERCENT_POWER_DEFAULT);
+    init_parameter_in_flash_uint32(TRIAC_MODE_KEY, TRIAC_MODE_DEFAULT);
+    
+    //init_date_parameter_in_flash(PWM_DATE_OFF_KEY, time_info_default);
+    
+}
+//------------------- DEFINICION DE FUNCIONES EXTERNAS -------------------------
+//------------------------------------------------------------------------------
+// TO DO: implementar funciones de escritura y lectura de date en flash
+void nv_flash_manager_init(void)
+{
+    nv_flash_driver_init();
+
+    nv_flash_driver_install_flash();
+
+    first_time_flash();
+}
+//------------------------------------------------------------------------------
+void init_date_parameter_in_flash(char *key, struct tm time_info_default)
+{
+    char buffer[50];
+    strcpy(buffer, "");
+
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &time_info_default);
+    init_parameter_in_flash_str(key, buffer);
+}
+//---------------------------- END OF FILE -------------------------------------
+//------------------------------------------------------------------------------
