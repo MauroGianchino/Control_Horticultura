fe1d9edf4ab14a224a7be8b5afcb70772988af80
gaston <gastonvallasciani@gmail.com>
2023-07-09 10:52:08 -0300

    nv_flash_manager done
    first time on nv flash done.
    
    New functions to read, write and init date info on nv flash.

--- src/nv_flash_manager.c
+++ src/nv_flash_manager.c
@@ -2,7 +2,6 @@
 //------------------------------------------------------------------------------
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -48,19 +47,21 @@ static void first_time_flash(void)
 
     init_parameter_in_flash_str(DEVICE_ALIAS_KEY, DEVICE_ALIAS_DEFAULT);
     init_parameter_in_flash_str(WIFI_AP_SSID_KEY, WIFI_AP_SSID_DEFAULT);
-
     init_date_parameter_in_flash(PWM_DATE_OFF_KEY, time_info_default);// work around
     init_date_parameter_in_flash(PWM_DATE_ON_KEY, time_info_default);
+    init_date_parameter_in_flash(TRIAC1_DATE_ON_KEY, time_info_default);
     time_info_default.tm_hour++;
     init_date_parameter_in_flash(PWM_DATE_OFF_KEY, time_info_default);
-
+    init_date_parameter_in_flash(TRIAC1_DATE_OFF_KEY, time_info_default);
+    time_info_default.tm_hour++;
+    init_date_parameter_in_flash(TRIAC2_DATE_ON_KEY, time_info_default);
+    time_info_default.tm_hour++;
+    init_date_parameter_in_flash(TRIAC2_DATE_OFF_KEY, time_info_default);
     init_parameter_in_flash_uint32(PWM_MODE_KEY, PWM_MODE_DEFAULT);
     init_parameter_in_flash_uint32(SIMUL_DAY_STATUS_KEY, PWM_SIMUL_DAY_STATUS_DEFAULT);
     init_parameter_in_flash_uint32(PWM_PERCENT_POWER_KEY, PWM_PERCENT_POWER_DEFAULT);
     init_parameter_in_flash_uint32(TRIAC_MODE_KEY, TRIAC_MODE_DEFAULT);
-    
-    //init_date_parameter_in_flash(PWM_DATE_OFF_KEY, time_info_default);
-    
+    init_parameter_in_flash_uint32(RELE_VEGE_STATUS_KEY, RELE_VEGE_STATUS_DEFAULT);
 }
 //------------------- DEFINICION DE FUNCIONES EXTERNAS -------------------------
 //------------------------------------------------------------------------------
@@ -77,10 +78,40 @@ void nv_flash_manager_init(void)
 void init_date_parameter_in_flash(char *key, struct tm time_info_default)
 {
     char buffer[50];
-    strcpy(buffer, "");
+
+    memset(buffer, 0, sizeof(buffer));
 
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &time_info_default);
     init_parameter_in_flash_str(key, buffer);
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+uint8_t read_date_from_flash(char *key, struct tm *time_info)
+{
+    char buffer[50];
+
+    memset(buffer, 0, sizeof(buffer));
+
+    read_parameter_from_flash_str(key);
+    if(wait_for_flash_response_str(buffer))
+    {
+        if(strptime(buffer, "%Y-%m-%d %H:%M:%S", time_info) == NULL) 
+        {
+            printf("Error al convertir la cadena a struct tm.\n");
+        }
+        return 1;
+    }
+    return 0;
+}
+//------------------------------------------------------------------------------
+void write_date_on_flash(char *key, struct tm time_info)
+{
+    char buffer[50];
+
+    memset(buffer, 0, sizeof(buffer));
+
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &time_info);
+    write_parameter_on_flash_str(key, buffer);  
 }
 //---------------------------- END OF FILE -------------------------------------
 //------------------------------------------------------------------------------
