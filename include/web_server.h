88bbbfaf1a53c186b973ad704212c0dc383d0114
Varian32 <Varian32@github.com>
2023-08-06 02:43:59 -0300

    remuevo estructuras innecesarias del web_server.h

--- include/web_server.h
+++ include/web_server.h
@@ -13,66 +13,11 @@
 #include <esp_system.h>
 #include "nvs_flash.h"
 
-typedef enum
-{
-    MANUAL,
-    AUTOMATICO,
-    APAGADO,
-    SI,
-    NO,
-    VEGE,
-    FLOR
-} modo_t;
-
-typedef struct horario
-{
-    int h;
-    int m;
-
-} horario_t;
-
 typedef struct red
 {
     char ID[33];   // maximo 32 caracteres
     char PASS[33]; // maximo 32 caracteres
 } red_t;
-
-typedef struct pwm
-{
-    int intensidad;
-    horario_t ih1;
-    horario_t fh1;
-    modo_t modo;
-    bool dia;
-} pwm_t;
-
-typedef struct triac
-{
-    modo_t modo;
-    horario_t ih1;
-    horario_t fh1;
-    horario_t ih2;
-    horario_t fh2;
-    horario_t ih3;
-    horario_t fh3;
-    horario_t ih4;
-    horario_t fh4;
-    bool checkh1;
-    bool checkh2;
-    bool checkh3;
-    bool checkh4;
-
-} triac_t;
-
-typedef struct vegeflor
-{
-    modo_t modo;
-} vegeflor_t;
-
-typedef struct hora
-{
-    horario_t hr;
-} hora_t;
 
 esp_err_t index_get_handler(httpd_req_t *);
 esp_err_t config_get_handler(httpd_req_t *);
